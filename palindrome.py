be8bb5a8742c6ebcdedbbfdb1082d6bf07546e3d
Erasmo <erasmo.cjunior@gmail.com>
2020-12-19 21:25:27 -0300

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,17 @@
+def first(word):
+    return word[0]
+
+
+def last(word):
+    return word[-1]
+
+
+def middle(word):
+    return word[1:-1]
+
+
+def is_palindrome(word):
+    if first(word) == last(word):
+        return True
+    else:
+        return False
